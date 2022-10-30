890a8e5f6295ec9b6df9458d66559768e7b07dd8
Nihal Abdul Khader MP <nihalmp45@gmail.com>
2022-10-30 15:19:15 +0530

    original version

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <div>
+         <div className='headerline'>
+            <img className='imgface' src='Logo (1).png'></img>
+            <span className='span'>React Course-project-3</span>
+         </div>
+    </div>
+  )
+}
+
+export default Header
